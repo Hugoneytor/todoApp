a7eee1b8df142f9403982794bc919933e20002a1
Hugo Alvarez <hugoneitor23@gmail.com>
2022-06-12 00:04:18 -0500

    input and states added

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoApp = () => {
 
-    return (
-      <div>Hola</div>
-    )
+  const [title, setTitle] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    if(title.length > 0){
+      const newTodo = {
+        id: crypto.randomUUID(),
+        title: title,
+        completed: false,
+      }
+
+      setTodos([...todos, newTodo]);
+      setTitle('');
+    }
+  }
+
+  return (
+    <div className='todoContainer'>
+      <form className='todoCreateForm' onSubmit={handleClick}>
+        <input 
+          className='todoInput' 
+          onChange={handleInputChange} value={title}/>
+        <input 
+          className='buttonCreate' 
+          type="submit" 
+          value="Create todo" 
+          onClick={ handleClick }/>
+      </form>
+      {title}
+    </div>
+  )
 }
 
 export default TodoApp
